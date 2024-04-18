8a9005ed8c4425723a4af885851502c22a2c068b
dani-lin-lp <dani.linux.97@gmail.com>
2024-04-18 14:38:08 +0200

    Change methods to async await

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,40 +19,68 @@ public_users.post("/register", (req, res) => {
 });
 
 // Get the book list available in the shop
-public_users.get("/", function (req, res) {
-  // Send the list of books as a JSON response
-  return res.status(200).json(books);
+public_users.get("/", async function (isbn) => {
+  try {
+    const book = books.find(book => book.isbn === isbn);
+    if (!book) {
+      throw new Error('Book not found');
+    }
+    return book;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 });
 
 // Get book details based on ISBN
-public_users.get("/isbn/:isbn", function (req, res) {
-  const isbn = req.params.isbn;
-  const book = Object.fromEntries(
-    Object.entries(books).filter(([key]) => key.includes(isbn))
-  );
-  if (book) {
-    return res.status(200).json(book);
-  } else {
-    return res.status(404).json({ message: "Book not found" });
+public_users.get("/isbn/:isbn", async function (req, res) {
+  try {
+    const isbn = req.params.isbn;
+    const book = Object.fromEntries(
+      Object.entries(books).filter(([key]) => key.includes(isbn))
+    );
+    if (book) {
+      return res.status(200).json(book);
+    } else {
+      throw new Error('Book not found');
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(404).json({ message: error.message });
   }
 });
 
 // Get book details based on author
-public_users.get("/author/:author", function (req, res) {
-  const author = req.params.author;
-  const bookKeys = Object.keys(books);
-  const matchingBooks = bookKeys.filter((key) => books[key].author === author);
-  if (matchingBooks.length > 0) {
-    return res.status(200).json(matchingBooks.map((key) => books[key]));
-  } else {
-    return res.status(404).json({ message: "No books found by this author" });
+public_users.get("/author/:author", async function (req, res) {
+  try {
+    const author = req.params.author;
+    const bookKeys = Object.keys(books);
+    const matchingBooks = bookKeys.filter((key) => books[key].author === author);
+    if (matchingBooks.length > 0) {
+      return res.status(200).json(matchingBooks.map((key) => books[key]));
+    } else {
+      throw new Error('No books found by this author');
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(404).json({ message: error.message });
   }
 });
 
 // Get all books based on title
-public_users.get("/title/:title", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+public_users.get("/title/:title", async function (req, res) {
+  try {
+    const title = req.params.title;
+    for (const [key, value] of Object.entries(books)) {
+      if (value.title === title) {
+        return res.status(200).json(books[key]);
+      }
+    }
+    throw new Error('Book not found');
+  } catch (error) {
+    console.error(error);
+    return res.status(404).json({ message: error.message });
+  }
 });
 
 //  Get book review
